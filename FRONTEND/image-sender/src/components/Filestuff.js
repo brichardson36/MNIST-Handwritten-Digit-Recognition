30e534c28b3aeab69c77dc6a3ea4bca0851abae6
rfaulkner7 <rfaulkner7@gatech.edu>
2021-02-03 12:42:14 -0500

    canvas

--- FRONTEND/image-sender/src/components/Filestuff.js
+++ FRONTEND/image-sender/src/components/Filestuff.js
@@ -1,23 +1,95 @@
-import React from "react"
+import React, { useEffect, useState, useRef } from "react"
 import "../App.css"
+import axios from "axios";
 
 export default function Filestuff (props){
 
     const [file, setFile] = React.useState()
+    const canvasRef = useRef(null)
+    const contextRef = useRef(null)
+    const [isDrawing, setIsDrawing] = useState(false)
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth/2;
+        canvas.height = window.innerHeight/2;
+        canvas.style.width = '${window.innerWidth}px';
+        canvas.style.height = '${window.innerHeight}px';
+
+        const context = canvas.getContext("2d")
+        // context.scale(2,2)
+        context.lineCap = "round"
+        context.strokeStyle = "black"
+        context.lineWidth = 5
+        contextRef.current = context
+    }, [])
 
     return(
         <div>
-            <h1>Input Number Image:</h1>
-            <input onChange = {(e) => handleFileChange(e)} type = "file" placeholder = "select file"/>
-            {/* <button onClick = {showFile}>Show curr File</button> */}
+            {/* <input onChange = {(e) => handleFileChange(e)} type = "file" placeholder = "select file"/> */}
+            <canvas
+            class = "canvasClass"
+            id = "canvas"
+            onMouseDown = {startDrawing}
+            onMouseUp = {finishDrawing}
+            onMouseMove = {draw}
+            ref = {canvasRef}
+            >
+            </canvas>
+            <p/>
+            <button onClick = {downloadImage}>Send Drawn Image</button>
         </div>
     );
 
     function handleFileChange(selected){
-        setFile(selected)
+        setFile(selected.target.files[0])
     }
 
-    // function showFile(){
-    //     console.log(file)
-    // }
+    function startDrawing({nativeEvent}){
+        const {offsetX, offsetY} = nativeEvent;
+        contextRef.current.beginPath()
+        contextRef.current.moveTo(offsetX, offsetY)
+        setIsDrawing(true)
+    }
+
+    function finishDrawing(){
+        contextRef.current.closePath()
+        setIsDrawing(false)
+    }
+
+    function draw({nativeEvent}){
+        if(!isDrawing){
+            return
+        }
+        const {offsetX, offsetY} = nativeEvent;
+        contextRef.current.lineTo(offsetX, offsetY)
+        contextRef.current.stroke()
+    }
+
+    function downloadImage(){
+        let downloadLink = document.createElement('a');
+        downloadLink.setAttribute('download', 'CanvasAsImage.png');
+        let canvas = document.getElementById('canvas');
+        let dataURL = canvas.toDataURL('image/png');
+        let url = dataURL.replace(/^data:image\/png/,'data:application/octet-stream');
+        downloadLink.setAttribute('href', url);
+        downloadLink.click();
+    }
+
+    function sendFile(){
+        if(file !== null){
+            let formData = new FormData()
+            formData.append('file', file)
+
+            axios.post(
+                "http://localhost:5000/predict/",
+                formData
+            ).then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
 }
